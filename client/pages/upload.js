0d88c90cf18266add0e07c6ed2e8b3dbb7f728b2
Muhammad Iqbal Masykuri <muhammadiqbal@synapsis.id>
2023-06-07 09:51:41 +0700

    fix

--- client/pages/upload.js
+++ client/pages/upload.js
@@ -77,9 +77,6 @@ export default function upload() {
 
             // Mengatur cookie baru dengan data yang diperbarui
             setCookie("history", newCookies);
-          })
-          .then(() => {
-            // Get the download URL of the uploaded image
             setIsModalOpen(true);
           })
           .catch((error) => {
